6612bbd8517114728a52afc3a251f30dad9559e0
apriqotgithub <orijeetmukherjee.im@gmail.com>
2025-07-08 12:43:45 -0400

    New changes
    done

--- src/integrations/client.ts
+++ src/integrations/client.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://eeifougcenzblqzcdqde.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlaWZvdWdjZW56YmxxemNkcWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MTAyNjgsImV4cCI6MjA2NzQ4NjI2OH0.-hHhnrqixR4hlZy821ewwdKvt80ABWeObKbfBiWFOKk';
-
+//Testing
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
