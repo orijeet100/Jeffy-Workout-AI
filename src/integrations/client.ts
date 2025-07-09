c9ae9877ac8696f61d63c5225f877f7ac37b7906
apriqotgithub <orijeetmukherjee.im@gmail.com>
2025-07-09 11:19:42 -0400

    Update client.ts

--- src/integrations/client.ts
+++ src/integrations/client.ts
@@ -1,7 +1,10 @@
 // Set up Google OAuth in your Supabase project dashboard under Authentication > Providers > Google.
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://eeifougcenzblqzcdqde.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlaWZvdWdjZW56YmxxemNkcWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MTAyNjgsImV4cCI6MjA2NzQ4NjI2OH0.-hHhnrqixR4hlZy821ewwdKvt80ABWeObKbfBiWFOKk';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase environment variables!');
+}
 //Testing
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
